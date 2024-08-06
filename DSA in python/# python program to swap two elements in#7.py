4a33567514f3f00d1e207ce427f43f9b2a8e4c67
SHIVAM DHUMAL <142675774+shivamdhumal77@users.noreply.github.com>
2024-08-06 07:29:26 +0530

    Add files via upload

---
+++ DSA in python/# python program to swap two elements in#7.py
@@ -0,0 +1,14 @@
+# python program to swap two elements in a list
+# Python3 program to swap elements
+# at given positions
+ 
+# Swap function
+
+def swapPositions(list, pos1, pos2):
+    list[pos1], list[pos2] = list[pos2], list[pos1]
+    return list
+
+list = [23,65,19,90]
+pos1 , pos2 = 1,3
+
+print(swapPositions(List, pos1-1, pos2-1))
